538ce20d17ba20f7f2192480b24c9a2e4d544f28
Yargo Siqueira <yargo.siqueira@redectsgi.local>
2021-04-29 13:50:13 -0300

    Inicial Basic App

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,15 @@
+<h1>Cadastrar Novo Post</h1>
+@if ($errors->any())
+    <div>
+        @foreach ($errors->all() as $error)
+        <li>{{$error}}</li>            
+        @endforeach
+    </div>
+@endif
+<form action="{{route('posts.store')}}" method="POST">
+    @csrf
+    <input type="text" name="title" id="title" placeholder="Título" value="{{@old('title')}}">
+    <textarea name="content" id="content" placeholder="Conteúdo">{{@old('content')}}</textarea>
+    <button type="submit"> Enviar </button>
+
+</form>
---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,5 @@
+<h3>Listagem</h3>
+@foreach ($posts as $post)
+    <p>{{$post->title}}</p>
+    
+@endforeach
